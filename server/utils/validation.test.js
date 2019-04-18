f74c813460950e8c5854032b91b70503a53cc7fd
Carlos Herrera <carlos.cervantes.cp@gmail.com>
2019-04-17 21:37:37 -0500

    Update snippet regions

--- server/utils/validation.test.js
+++ server/utils/validation.test.js
@@ -2,21 +2,27 @@ const expect = require('expect');
 const { isRealString } = require('./validation.js');
 
 describe('isRealString', () => {
+  /** @region_snippet_RejectNonString */
   it('Should reject non-string values', () => {
     var response = isRealString(98);
 
     expect(response).toBe(false);
   });
+  /** @endregion */
 
+  /** @region_snippet_TestRejectWhiteSpaces */
   it('Should reject strings with only spaces', () => {
     var response = isRealString('    ');
 
     expect(response).toBe(false);
   });
+  /** @endregion */
 
+  /** @region_snippet_TestAllowStringsWithNonSpace */
   it('Should allow strings with non-space characters', () => {
     var response = isRealString('   Carlos   ');
 
     expect(response).toBe(true);
   });
+  /** @endregion */
 });
